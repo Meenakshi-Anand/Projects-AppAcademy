9b4463598f6d94f4abb75edb21010aa637609c50
Meenakshi <abhi@codeanand.com>
2018-04-23 18:07:54 -0700

    End of day1

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,15 +1,32 @@
 class Piece
+attr_reader :name
+  # def initialize(position, name)
+  #   @name = name
+  #   @position = position
+  # end
+
+  def moves
+
+  end
 
 end
 
-class NullPiece < Piece
-
-end
 
 module SlidingPiece
 
+  def move_dirs
+
+  end
 end
 
-module SteppingPiece 
+module SteppingPiece
 
+end
+
+require 'singleton'
+class NullPiece < Piece
+
+  def initialize
+
+  end
 end
